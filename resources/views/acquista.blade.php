b72978639de66b8017e126db05d088cc7c858c77
Francesco Voto <S1087880@studenti.univpm.it>
2021-06-01 19:12:52 +0200

    acquista

--- resources/views/acquista.blade.php
+++ resources/views/acquista.blade.php
@@ -5,45 +5,52 @@
 @section('content')
 
 
-             <div id="tooplate_content">
-               
-                 
-                 <form name="miaform">
-                 <link rel="stylesheet" type="text/css" href="{{asset('css/bottoni.css')}}" />
-                 @isset($event)
-                 
-              <h2>Acquisto del biglietto: {{ $event->nomeEvento }}</h2>
-              
-               
-               <p>Data/Ora:{{ $event->dataOra }}</p>
-                   <br>
-              <p>Prezzo biglietto :{{ $event->prezzo }}</p>
-                  <br>
-                   
-                    <p>Numero di biglietti da acquistare:<input name="someid"  value="1" min="1" max="{{ $event->bigliettiDisponibili }}" type="number" onkeypress="return isNumberKey(evt)"></p>
-                    
-                    <p><input type="radio" name="linguaggio"  value="carta"> Carta di credito</p>
-                    <p><input type="radio" name="linguaggio" value="html">cPaypal</p>
-                    <p><input type="radio" name="linguaggio" value="html"/> Conto corrente</p>
-                    <script type="text/javascript">
-                  
-                        window.onload = function () {
-                document.getElementById('someid').onchange = function () {
-                    getProvincia(this.options[this.selectedIndex].text);
-                };
-                };
-                    
-                    </script>
-                    
-                    <h2>Totale : {{ $event->prezzo}}</h2>
+<div id="tooplate_content">
+    <script type="text/javascript">
+        $(function () {
+            $('#someid')
+                    .on('click', function (event) {
+                        var prezzo = $('#tiket').text() * $(this).val();
+                        putprice(prezzo);
+                    });
+        });
+        function putprice(text) {
+            $('#prezzo').val(text);
+        }
+    </script>
+
+    <form name="miaform">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/bottoni.css')}}" />
+        @isset($event)
+
+        <h2>Acquisto del biglietto: {{ $event->nomeEvento }}</h2>
+
+
+        <p>Data/Ora:{{ $event->dataOra }}</p>
+        <br>
+        <p >Prezzo biglietto :<b id="tiket">{{ $event->prezzo }}</b></p>
+        <br>
+
+        <p id="console">Numero di biglietti da acquistare:<input name="someid" id="someid" value="1" min="1" max="{{ $event->bigliettiDisponibili }}" type="number" onkeypress="return isNumberKey(evt)"></p>
+
+        <p><input type="radio" name="linguaggio"  value="carta"> Carta di credito</p>
+        <p><input type="radio" name="linguaggio" value="html">Paypal</p>
+        <p><input type="radio" name="linguaggio" value="html"/> Conto corrente</p>
+
+
+        <br><br>
+
+        <label><b>Totale:<b><input type="number" value="{{ $event->prezzo}}" id="prezzo" readonly>€</label>
+                    <br><br>
+
                     <input type="button" class="input" value=paga> 
-                 
-            </form>
-               <div class="cleaner h20"></div>
-                   
-              <br class="cleaner" />
-               
-               @endisset()
-             
-                 </div>
-@endsection
+
+                    </form>
+                    <div class="cleaner h20"></div>
+
+                    <br class="cleaner" />
+
+                    @endisset()
+
+                    </div>
+                    @endsection
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,6 +5,10 @@
         <link href="{{asset('css/tooplate_style.css')}}" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="{{asset('css/ddsmoothmenu.css')}}" />
         
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        
         
     </head>
     <body>
